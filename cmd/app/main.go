41bb1f1255d6d2ec00a52be7d2a8d2115a5f11d5
lemiam <qwazarzip@gmail.com>
2024-11-13 11:24:23 +0300

    fix some problems

---
+++ cmd/app/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"eth_bal/configs"
+	"eth_bal/internal/app"
+	"eth_bal/pkg/log"
+	"fmt"
+
+	"time"
+)
+
+func main() {
+	startTime := time.Now()
+
+	cfg, err := configs.LoadConfig("configs/config.yml")
+	if err != nil {
+		log.Logger.Errorf("Ошибка загрузки конфигурации: %v", err)
+		return
+	}
+
+	if err := app.Run(cfg); err != nil {
+		log.Logger.Errorf("Ошибка запуска приложения: %v", err)
+	}
+
+	duration := time.Since(startTime)
+	fmt.Printf("Программа выполнялась: %v\n", duration)
+}
